7ea19bec110284d35ff2f09e1174b584238edad4
yash-vargaonkar <yashvargaonkar@gmail.com>
2024-10-22 22:36:25 +0530

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+Now execute the main.js to start the server as follows −
+// $ node main.js
+// Verify the Output. Server has started.
+// Server running at http://127.0.0.1:8081/
+// Make a Request to the Node.js Server
+// Open http://127.0.0.1:8081/ in any browser and observe the following result.
